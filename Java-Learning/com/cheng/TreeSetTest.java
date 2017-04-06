8b7dc76759bbe5994debdf2a31e0f5990c9253ec
cheng <shencheng.wan@outlook.com>
2017-04-06 23:58:04 +0800

    update

---
+++ Java-Learning/com/cheng/TreeSetTest.java
@@ -0,0 +1,14 @@
+
+
+import java.util.TreeSet;
+
+public class TreeSetTest {
+	public static void main(String[] args) {
+		TreeSet ts = new TreeSet();
+		ts.add(5);
+		ts.add(2);
+		ts.add(9);
+		ts.add(-10);
+		System.out.println(ts);
+	}
+}
